7550aedc55ddc2f35bf78832d4d09b9a1a517058
rood <nprudnov@outlook.com>
2023-02-10 22:51:08 +0200

    12.02

--- books/urls.py
+++ books/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('all_books', views.all_books, name='all_books'),
     path('all_authors', views.all_authors, name='all_authors'),
+    path('delete_authors/<int:id>', views.del_authors, name='delete_authors'),
+    path('delete_books/<int:id>', views.del_book, name='delete_book'),
     path('more/<int:pk>', views.more, name='more'),
     path('add_author/', views.add_author, name='add_author'),
     path('add_book/', views.add_book, name='add_book'),
+
 
     ]
 
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
@@ -39,8 +40,11 @@ def add_author(request):
         'form': form
 
     }
-    if request.method == 'POST':
-        form = AuthorForm(request.POST, request.FILES)
+    if not request.user.is_superuser:
+        if request.method == 'POST':
+            form = AuthorForm(request.POST, request.FILES)
+        else:
+            return HttpResponse('You are do not superuser status')
         if form.is_valid():
             author = Author.objects.create(**form.cleaned_data)
 
@@ -48,16 +52,29 @@ def add_author(request):
 
     return render(request, 'author_form.html', context=context)
 
-
+@login_required
 def add_book(request):
     form = BookForm
     context = {
         'form': form
     }
-    if request.method == "POST":
-        form = BookForm(request.POST, request.FILES)
+    if not request.user.is_superuser:
+        if request.method == "POST":
+            form = BookForm(request.POST, request.FILES)
+        else:
+            return HttpResponse('You are do not superuser status')
         if form.is_valid():
             book = Book.objects.create(**form.cleaned_data)
             return HttpResponse(f'Book {book} is add')
     return render(request, 'book_form.html', context=context)
+
+def del_authors(request, id):
+    authors = get_object_or_404(Author, id=id)
+    authors.delete()
+    return redirect(reverse_lazy('all_authors'))
+
+def del_book(request, id):
+    book = get_object_or_404(Book, id=id)
+    book.delete()
+    return redirect(reverse_lazy('all_books'))
 
